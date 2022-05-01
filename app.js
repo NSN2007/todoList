6f862b1c7e4e5954c91d7e6cc636d98f34c601f4
Atlas <68791999+Atlas7005@users.noreply.github.com>
2022-05-01 13:37:13 +0200

    :)

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const port = 80;
-var todo = fs.existsSync("todo.json") ? JSON.parse(fs.readFileSync("todo.json")) : [];
 
 app.use(express.static('public'));
 app.use(express.json());
 
 app.post('/test', (req, res) => {
     if(!req.body.todo || req.body.todo.length < 1) return res.status(400).send("Missing parameter");
+    var todo = fs.existsSync("todo.json") ? JSON.parse(fs.readFileSync("todo.json")) : [];
 
     todo.push(req.body.todo);
-    fs.writeFileSync("todo.json", JSON.stringify(todo), { flag: "a+" });
+    fs.writeFileSync("todo.json", JSON.stringify(todo));
 });
 
 app.get("/todo", (req, res) => {
+    var todo = fs.existsSync("todo.json") ? JSON.parse(fs.readFileSync("todo.json")) : [];
     res.json(todo);
 });
 
